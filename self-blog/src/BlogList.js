f8cb91e48b2ef966473020eac1e151eefc00f32d
root <root@SharonsLaptop.localdomain>
2024-12-24 19:28:38 +0800

    add: add delete function as props

--- self-blog/src/BlogList.js
+++ self-blog/src/BlogList.js
@@ -1,6 +1,7 @@
 const BlogList = (props) => {
     const blogs = props.blogs
     const title = props.title
+    const handleDelete = props.handleDelete
     // console.log(props)
     // console.log(props.blogs)
     return ( 
@@ -9,6 +10,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick = {()=> handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
             
--- self-blog/src/home.js
+++ self-blog/src/home.js
@@ -4,7 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: "First", body: "1", author: "Sharon", id: 1}, 
-        { title: "Second", body: "2", author: "Sharon", id: 2}, 
+        { title: "Second", body: "2", author: "Rob", id: 2}, 
         { title: "Third", body: "3", author: "Sharon", id: 3}
     ]);
 
@@ -25,15 +25,38 @@ const Home = () => {
             console.log("Welcome " + name + " !")
          );
     }
-     
+    
+    // 正向作法
+    const handleDelete = (id) => {
+        const index = blogs.findIndex(blog => blog.id === id);
+        const deletedBlogs = [...blogs];
+        // 如果 index 存在 ( index === -1 表示元素不存在)
+        if (index !== -1) {
+            deletedBlogs.splice(index, 1);
+            // console.log("deletedBlogs")
+            // console.log(deletedBlogs)
+            // console.log("blogs")
+            // console.log(blogs)
+            setBlogs(deletedBlogs);
+        }         
+    }
+
+    //反向做法
+    // const handleDelete = (id) => {
+    //     const deletedBlogs = blogs.filter(blog => blog.id !== id);
+    //     console.log(deletedBlogs);
+    //     setBlogs(deletedBlogs);
+    // }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             {/* <button onClick = {click_counts()}>Click Counts</button> */}
             {/* <button onClick = {click_counts}>Click Counts</button> */}
-            <button onClick = {()=> welcome_message('Mr. Nobody')}>Hidden Message</button>
-            <Click_counts / >
-            <BlogList blogs = {blogs} title = "All Blogs!" / >
+            {/* <button onClick = {()=> welcome_message('Mr. Nobody')}>Hidden Message</button>
+            <Click_counts / > */}
+            {/* <BlogList blogs = {blogs} title = "All Blogs!" / > */}
+            <BlogList blogs = {blogs}  title = "Rob's Blogs!" handleDelete = {handleDelete}/ >
         </div>
      );
 }
